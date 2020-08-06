dbe2fff4112ba3ea8e7ad875a9ae56e020ff22b6
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-06 01:02:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,9 +3,8 @@ class CashRegister
   attr_accessor :total
   def initialize(total = 0)
     @total = total
-  end
-  def discount
-     self.total = 20
+    @items = []
+    @discount = discount
   end
   def add_item(title, price)
     
